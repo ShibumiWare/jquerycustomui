0e8536e057761897c850a601c60c734da6b4256f
ShibumiWare <shibumi@shibumiware.com>
2018-05-31 22:52:08 -0500

    Create pfoot.js

---
+++ pfoot.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function () {
+
+    if (window.location.href !== 'http://www.red-stone-rising.com/') {
+        $('.column-right-outer').hide();
+        $('.fauxcolumn-inner').hide();
+        $('.column-center-inner').css('width', '800px');
+        $('.post-outer').css('width', '800px');
+        $('.post-outer').css('padding-left', '50px');
+        $('.blog-pager').hide();
+        $('#HTML1').hide();
+    } else {
+        $('.column-right-outer').show();
+        $('.fauxcolumn-inner').show();
+        $('.blog-pager').show();
+        $('#HTML1').show();
+    }
+});
+
